a54d7d17f3c410c055ff90cd1941fceec9a2145e
Lucian <dracea.lucian.mihai@gmail.com>
2022-10-16 21:54:55 +0300

    Added a Test Pipeline for EasyOpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/detection/TestPipeline.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.rasky.detection;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+/**
+ * Test pipeline for EOCV Sim.
+ *
+ * @author Lucian
+ * @version 1.0
+ */
+public class TestPipeline extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
