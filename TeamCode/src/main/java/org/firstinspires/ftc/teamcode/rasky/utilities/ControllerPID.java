8b30b22d22c19bec5bb4e96b9d2568629678e05a
Lukyen <dracea.lucian.mihai@gmail.com>
2022-11-05 07:30:43 +0200

    Refactored & Modified a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/utilities/ControllerPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/utilities/ControllerPID.java
@@ -7,36 +7,40 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * A basic PID controller.
  *
  * @author Lucian
- * @version 1.0
+ * @version 1.1
  */
 public class ControllerPID {
-    double prevError = 0;
-    double integral = 0;
-    boolean started = false;
-
-    ElapsedTime timer = new ElapsedTime();
-
-    double Kp, Ki, Kd;
+    double kP, kI, kD;
 
     public ControllerPID(double Kp, double Ki, double Kd) {
-        this.Kp = Kp;
-        this.Ki = Ki;
-        this.Kd = Kd;
+        this.kP = Kp;
+        this.kI = Ki;
+        this.kD = Kd;
     }
 
+    double prevError = 0;
+    double integral = 0;
     public double calculate(double reference, double state) {
         double error = reference - state;
+
         double dt = getDt();
         double derivative = (error - prevError) / dt;
+
         integral += ((error + prevError) / 2) * dt;
+
         prevError = error;
-        return Kp * error + Ki * integral + Kd * derivative;
+        return kP * error + kI * integral + kD * derivative;
     }
 
-    public double calculate(double error) {
-        return calculate(0, -error);
-    }
+    boolean started = false;
+    ElapsedTime timer = new ElapsedTime();
 
+    /**
+     * This method is responsible for starting the PID controller timer correctly and
+     * getting the time between the loops for the derivative value.
+     *
+     * @return Returns time between last and current loop
+     */
     private double getDt() {
         if (!started) {
             started = true;
